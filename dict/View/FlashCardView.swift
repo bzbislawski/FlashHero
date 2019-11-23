0b59e4cef0ed15c2464fd281645b5dd404d8dff8
Bartosz Zbislawski <bzbislawski@gmail.com>
2019-11-23 16:19:04 +0000

    Move code to flashcard and flashcarddeck

--- dict/View/FlashCardView.swift
+++ dict/View/FlashCardView.swift
@@ -11,30 +11,68 @@ import SwiftUI
 struct FlashCardView: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @Binding var showAnswer: Bool
+    @State private var currentPosition: CGSize = .zero
+    @State private var newPosition: CGSize = .zero
     var word: String
     var translation: String
     
+    var animation: Animation {
+        Animation.interpolatingSpring(mass: 3, stiffness: 80, damping: 10, initialVelocity: 0)
+    }
+    
+    func textView(text: String, isAnswer: Bool) -> some View {
+        Text(text)
+            .bold()
+            .font(.title)
+            .rotation3DEffect(Angle(degrees: isAnswer ? 180 : 0), axis: (x: 1, y: 0, z: 0))
+            .offset(x: self.currentPosition.width, y: self.currentPosition.height)
+            .rotation3DEffect(.degrees(self.showAnswer ? 180 : 0), axis: (x: 1, y: 0, z: 0))
+            .onTapGesture {
+                print(self.showAnswer)
+                self.showAnswer.toggle()
+        }
+        .animation(self.animation)
+        .gesture(DragGesture()
+        .onChanged { value in
+            self.currentPosition = CGSize(width: value.translation.width + self.newPosition.width, height: self.showAnswer ? -1 * value.translation.height : value.translation.height + self.newPosition.height)
+        }
+        .onEnded { value in
+            self.currentPosition = CGSize.zero
+            print(self.newPosition.width)
+            self.newPosition = self.currentPosition
+        })
+    }
+    
     var body: some View {
         ZStack {
+            self.textView(text: self.translation, isAnswer: true)
+                .zIndex(self.showAnswer ? 1 : 0)
+            
             Image("rectangle")
                 .resizable()
                 .cornerRadius(10)
                 .frame(width: 320, height: 220)
                 .overlay(RoundedRectangle(cornerRadius: 10).stroke(self.colorScheme == .light ? Color.white : Color.gray, lineWidth: 4))
                 .shadow(radius: 5)
+                .offset(x: self.currentPosition.width, y: self.currentPosition.height)
+                .rotation3DEffect(.degrees(self.showAnswer ? 180 : 0), axis: (x: 1, y: 0, z: 0))
+                .onTapGesture {
+                    print(self.showAnswer)
+                    self.showAnswer.toggle()
+            }
+            .animation(self.animation)
+            .gesture(DragGesture()
+            .onChanged { value in
+                self.currentPosition = CGSize(width: value.translation.width + self.newPosition.width, height: self.showAnswer ? -1 * value.translation.height : value.translation.height + self.newPosition.height)
+            }
+            .onEnded { value in
+                self.currentPosition = CGSize.zero
+                print(self.newPosition.width)
+                self.newPosition = self.currentPosition
+            })
             
-            Text(self.word)
-                .bold()
-                .font(.title)
-//                .opacity(self.showAnswer ? 0 : 1)
-            
-            Text(self.translation)
-              
-                .bold()
-                .font(.title)
-//                .opacity(showAnswer ? 1 : 0)
-                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
-//                .offset(x: 0, y: 60)
+            self.textView(text: self.word, isAnswer: false)
+                .zIndex(self.showAnswer ? -1 : 0)
         }
     }
 }
--- dict/View/Game.swift
+++ dict/View/Game.swift
@@ -11,73 +11,15 @@ import SwiftUI
 struct Game: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @State var showAnswer = false
-    @State private var currentPosition: CGSize = .zero
-    @State private var newPosition: CGSize = .zero
     
-    var animation: Animation {
-        Animation.interpolatingSpring(mass: 1, stiffness: 80, damping: 10, initialVelocity: 0)
-    }
-    
-    func textView(text: String, isAnswer: Bool) -> some View {
-        Text(text)
-            .bold()
-            .font(.title)
-            .rotation3DEffect(Angle(degrees: isAnswer ? 180 : 0), axis: (x: 1, y: 0, z: 0))
-            .offset(x: self.currentPosition.width, y: self.currentPosition.height)
-            .rotation3DEffect(.degrees(self.showAnswer ? 180 : 0), axis: (x: 1, y: 0, z: 0))
-            .onTapGesture {
-                print(self.showAnswer)
-                self.showAnswer.toggle()
-            }
-            .animation(self.animation)
-            .gesture(DragGesture()
-                .onChanged { value in
-                    self.currentPosition = CGSize(width: value.translation.width + self.newPosition.width, height: self.showAnswer ? -1 * value.translation.height : value.translation.height + self.newPosition.height)
-                }
-                .onEnded { value in
-                    self.currentPosition = CGSize.zero
-                    print(self.newPosition.width)
-                    self.newPosition = self.currentPosition
-                })
-    }
     
     var body: some View {
-        
-        GeometryReader { geometry in
-            ZStack {
-                FlashCardView(showAnswer: self.$showAnswer, word: "Kot", translation: "Answer").offset(y: -30).scaleEffect(0.95)
-                ZStack {
-                        self.textView(text: "Psiaczek", isAnswer: true)
-                            .zIndex(self.showAnswer ? 1 : 0)
-                    
-                        Image("rectangle")
-                            .resizable()
-                            .cornerRadius(10)
-                            .frame(width: 320, height: 220)
-                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(self.colorScheme == .light ? Color.white : Color.gray, lineWidth: 4))
-                            .shadow(radius: 5)
-                            .offset(x: self.currentPosition.width, y: self.currentPosition.height)
-                            .rotation3DEffect(.degrees(self.showAnswer ? 180 : 0), axis: (x: 1, y: 0, z: 0))
-                            .onTapGesture {
-                                print(self.showAnswer)
-                                self.showAnswer.toggle()
-                            }
-                            .animation(self.animation)
-                            .gesture(DragGesture()
-                            .onChanged { value in
-                                self.currentPosition = CGSize(width: value.translation.width + self.newPosition.width, height: self.showAnswer ? -1 * value.translation.height : value.translation.height + self.newPosition.height)
-                            }
-                            .onEnded { value in
-                                self.currentPosition = CGSize.zero
-                                print(self.newPosition.width)
-                                self.newPosition = self.currentPosition
-                            })
-                    
-                        self.textView(text: "Mietek", isAnswer: false)
-                            .zIndex(self.showAnswer ? -1 : 0)
-                }
-                
-            }
+        ZStack {
+            FlashCardDeckView().offset(y: -100).scaleEffect(0.80)
+            FlashCardDeckView().offset(y: -75).scaleEffect(0.85)
+            FlashCardDeckView().offset(y: -50).scaleEffect(0.90)
+            FlashCardDeckView().offset(y: -25).scaleEffect(0.95)
+            FlashCardView(showAnswer: self.$showAnswer, word: "Mietek", translation: "Piesel")
         }
     }
 }
