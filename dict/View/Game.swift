caf55403071d583ea52d07e5280b6e982246fb43
Bartosz Zbislawski <bzbislawski@gmail.com>
2019-12-22 00:44:32 +0000

    Initial changes to scroll view

--- dict/View/Game.swift
+++ dict/View/Game.swift
@@ -29,75 +29,20 @@ struct Game: View {
     }
     
     var body: some View {
+        VStack {
+            ForEach(0..<flashCards.count) { index in
+                FlashCardView(word: self.flashCards[index].word, translation: self.flashCards[index].translation)
+                    .animation(.spring())
+                    .padding(.top)
+            }
+            Spacer()
+        }.navigationBarTitle("Swipe game")
         
-        NavigationView {
-            VStack {
-                Spacer()
-                ZStack {
-                    ForEach(0..<flashCards.count) { index in
-                        FlashCardView(word: self.flashCards[index].word, translation: self.flashCards[index].translation)
-                            .offset(self.offset(index: index))
-                            .scaleEffect(self.scaleEffect(index: index))
-                            .animation(.spring())
-                    }
-                }
-                
-                Divider()
-                HStack {
-                    ScoreTile(text: "Current score", number: 20)
-                    ScoreTile(text: "Highscore", number: 35)
-                }
-                
-            }.navigationBarTitle("Swipe game")
-        }
     }
 }
 
 struct Game_Previews: PreviewProvider {
     static var previews: some View {
-        HStack {
-            ScoreTile(text: "Current score", number: 20)
-            ScoreTile(text: "Highscore", number: 35)
-        }
-    }
-}
-
-struct ScoreTile: View {
-    var text: String
-    var number: Int
-    
-    var body: some View {
-        
-        ZStack {
-            Image("rectangle_v2")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                
-                .frame(width:144, height: 144)
-                .background(Color(red:0.56, green:0.56, blue:0.58))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 5))
-                .cornerRadius(20)
-                .padding(.bottom, 20)
-                .padding()
-                .shadow(radius: 20, x: 0, y: 20)
-                .opacity(0.5)
-                .overlay(
-                    VStack(alignment: .leading) {
-                        Text(self.text)
-                            .font(.subheadline)
-                            .foregroundColor(.white)
-                            .padding(.top, -50)
-                            .padding(.leading, -30)
-                        Text("\(self.number)")
-                            .font(.title)
-                            .foregroundColor(.white)
-                            .padding(.top, -10)
-                        
-                    }.frame(width:144, height: 144))
-            
-            
-            
-        }
-        
+        Game()
     }
 }
