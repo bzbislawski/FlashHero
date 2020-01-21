4c09c12b66182eb8f989547e705832bbbac783c8
Bartosz Zbislawski <bzbislawski@gmail.com>
2020-01-21 22:25:19 +0000

    Add extension class, add Deck form

---
+++ dict/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  dict
+//
+//  Created by Bartosz Zbislawski on 21/01/2020.
+//  Copyright © 2020 Bartosz Zbislawski. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static func rgb(r: Double, g: Double, b: Double) -> Color {
+        return Color(red: r / 255, green: g / 255, blue: b / 255)
+    }
+    
+    static let backgroundColor = Color.rgb(r: 216, g: 216, b: 216)
+    static let secondaryBackgroundColor = Color.rgb(r: 200, g: 200, b: 200)
+    static let firstColor = Color.rgb(r: 10, g: 94, b: 74)
+}
+
+extension UIColor {
+    static let secondaryBackgroundColor = UIColor(red:0.75, green:0.75, blue:0.75, alpha:1.0)
+}
--- dict/View/DeckFormView.swift
+++ dict/View/DeckFormView.swift
@@ -9,9 +9,91 @@
 import SwiftUI
 
 struct DeckFormView: View {
+    @Environment (\.presentationMode) var presentationMode
+    @State private var name: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color.backgroundColor.edgesIgnoringSafeArea(.all)
+                VStack(spacing: 70) {
+                    VStack {
+                        Image(systemName: "book")
+                            .frame(width: 32, height: 32)
+                            .font(.system(size: 32, weight: .bold))
+                            .foregroundColor(Color("GreenFont"))
+                        Text("Deck")
+                            .foregroundColor(Color("GreenFont"))
+                            .font(.system(size: 32, weight: .bold))
+                    }
+                    VStack {
+                        HStack {
+                            Text("Deck name")
+                                .padding(.leading, 5)
+                                .foregroundColor(Color("GreenFont"))
+                                .font(.system(size: 18, weight: .semibold))
+                                Spacer()
+                        }.frame(width: 250)
+                        
+                        ZStack {
+                            TextField("", text: $name)
+                                .frame(width: 250, height: 40)
+                                .background(Color.white)
+                                .cornerRadius(5)
+                            HStack {
+                                Spacer()
+                                Button(action:
+                                {
+                                    self.name = ""
+                                })
+                                {
+                                    Image(systemName: "xmark.circle.fill")
+                                        .foregroundColor(Color.backgroundColor)
+                                }
+                                .padding(.trailing, 8)
+                            }.frame(width: 250)
+                        }
+                    }
+                    Button(action: {
+                        print("save")
+                    }) {
+                        VStack {
+                            Text("Save")
+                                .frame(width: 250, height: 40)
+                                .font(.system(size: 16, weight: .medium))
+                                .foregroundColor(Color.white)
+                        }
+                        .background(Color.firstColor)
+                        .cornerRadius(5)
+                    }.padding(.bottom, 25)
+                    Spacer()
+                    
+                }.navigationBarTitle("", displayMode: .inline)
+                .background(NavigationConfigurator { nc in
+                    nc.navigationBar.barTintColor = UIColor.secondaryBackgroundColor
+                })
+                .navigationBarItems(
+                    leading:
+                        Button(action: { self.presentationMode.wrappedValue.dismiss() }) { Text("Cancel").foregroundColor(Color("GreenFont")) }
+                )
+                    
+            }
+        }
     }
+}
+
+struct NavigationConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void = { _ in }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let nc = uiViewController.navigationController {
+            self.configure(nc)
+        }
+    }
+
 }
 
 struct DeckFormView_Previews: PreviewProvider {
