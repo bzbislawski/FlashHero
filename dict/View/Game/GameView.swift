6daadb6196a3172c35955f795992b75069a21be5
Bartosz Zbislawski <bartoszzbislawski@rentalcars.com>
2020-03-15 16:10:10 +0000

    Fixing layout

--- dict/View/Game/GameView.swift
+++ dict/View/Game/GameView.swift
@@ -20,8 +20,8 @@ struct GameView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                Text("Swipe Game")
-                    .font(.system(size: 44, weight: .thin))
+//                Text("Swipe Game")
+//                    .font(.system(size: 44, weight: .thin))
                 
                 ZStack {
                     Rectangle()
@@ -30,103 +30,102 @@ struct GameView: View {
                             Circle()
                                 .fill(Color.white)
                                 .frame(width: proxy.size.width + 1000, height: proxy.size.height + 1000)
-                                .offset(x: (proxy.size.width + 1000) * 0.05, y: -(proxy.size.height + 1000) * 0.50)
+                                .offset(x: (proxy.size.width + 1000) * 0.05, y: -(proxy.size.height + 1000) * 0.52)
                                 .opacity(0.15)
                             
                         })
-                        .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.8)
-                        .cornerRadius(5).shadow(radius:5)
+                        .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.96)
+                        .cornerRadius(25)
+                        .shadow(color: .gray, radius: 12, x: 0, y: 4)
                     
                     
                     
-                    //                    VStack {
-                    //                        Image(systemName: "gamecontroller")
-                    //                            .resizable()
-                    //                            .aspectRatio(contentMode: .fit)
-                    //                            .font(.system(size: 56, weight: .bold))
-                    //                            .frame(width: 100, height: 100)
-                    //                            .foregroundColor(.white)
-                    //                            .padding(.top, 60)
-                    //
-                    //                        HStack {
-                    //                            Text("Vocabulary")
-                    //                                .italic()
-                    //                                .padding(.leading, 30)
-                    //                                .foregroundColor(.white)
-                    //                                .font(.system(size: 36, weight: .thin))
-                    //
-                    //                            Spacer()
-                    //                        }
-                    //
-                    //                        HStack {
-                    //                            Text("Choose decks you want to play")
-                    //                                .italic()
-                    //                                .padding(.leading, 30)
-                    //                                .foregroundColor(.white)
-                    //                            Spacer()
-                    //                        }
-                    //                        Rectangle()
-                    //                            .fill(Color.white)
-                    //                            .cornerRadius(8)
-                    //                            .frame(height: 47)
-                    //                            .overlay(
-                    //                                RoundedRectangle(cornerRadius: 8)
-                    //                                    .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
-                    //                        )
-                    //                            .padding(.leading, 30)
-                    //                            .padding(.trailing, 30)
-                    //
-                    //                            .onTapGesture {
-                    //                                self.showSheet = true
-                    //                        }
-                    //
-                    //                        HStack {
-                    //                            Text("Word Order")
-                    //                                .italic()
-                    //                                .padding(.leading, 30)
-                    //                                .foregroundColor(.white)
-                    //                                .font(.system(size: 36, weight: .thin))
-                    //
-                    //                            Spacer()
-                    //                        }
-                    //
-                    //                        HStack {
-                    //                            Text("Choose order of words displayed")
-                    //                                .italic()
-                    //                                .padding(.leading, 30)
-                    //                                .foregroundColor(.white)
-                    //                            Spacer()
-                    //                        }
-                    //                        Rectangle()
-                    //                            .fill(Color.white)
-                    //                            .cornerRadius(8)
-                    //                            .frame(height: 47)
-                    //                            .overlay(
-                    //                                RoundedRectangle(cornerRadius: 8)
-                    //                                    .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
-                    //                        )
-                    //                            .padding(.leading, 30)
-                    //                            .padding(.trailing, 30)
-                    //
-                    //                        Spacer()
-                    //
-                    //                        Button(action: {}) {
-                    //                            RoundedRectangle(cornerRadius: 8)
-                    //                                .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
-                    //                                .background(Color.white)
-                    //                                .cornerRadius(8)
-                    //                                .frame(height: 47)
-                    //                                .overlay(
-                    //                                    Text("Start Game")
-                    //                                        .foregroundColor(Color.quaternaryBackgroundColor)
-                    //                                        .font(.system(size: 17, weight: .semibold))
-                    //                            )
-                    //                                .padding(.leading, 60)
-                    //                                .padding(.trailing, 60)
-                    //                                .padding(.top, 80)
-                    //                                .padding(.bottom, 80)
-                    //                        }
-                    //                    }
+                    VStack {
+                        Image(systemName: "gamecontroller")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .font(.system(size: 56, weight: .bold))
+                            .frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            .foregroundColor(.white)
+                            .padding(.bottom, geometry.size.height * 0.06)
+                        
+                        HStack {
+                            Text("Vocabulary")
+                                .italic()
+                                .padding(.leading, 30)
+                                .foregroundColor(.white)
+                                .font(.system(size: 36, weight: .thin))
+                            
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("Choose decks you want to play")
+                                .italic()
+                                .padding(.leading, 30)
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                        Rectangle()
+                            .fill(Color.white)
+                            .cornerRadius(8)
+                            .frame(height: 47)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
+                        )
+                            .padding(.leading, 30)
+                            .padding(.trailing, 30)
+                            
+                            .onTapGesture {
+                                self.showSheet = true
+                        }
+                        
+                        HStack {
+                            Text("Word Order")
+                                .italic()
+                                .padding(.leading, 30)
+                                .foregroundColor(.white)
+                                .font(.system(size: 36, weight: .thin))
+                            
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("Choose order of words displayed")
+                                .italic()
+                                .padding(.leading, 30)
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                        Rectangle()
+                            .fill(Color.white)
+                            .cornerRadius(8)
+                            .frame(height: 47)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
+                        )
+                            .padding(.leading, 30)
+                            .padding(.trailing, 30)
+                        
+                        Spacer()
+                        
+                        Button(action: {}) {
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
+                                .background(Color.white)
+                                .cornerRadius(8)
+                                .frame(height: 47)
+                                .overlay(
+                                    Text("Start Game")
+                                        .foregroundColor(Color.quaternaryBackgroundColor)
+                                        .font(.system(size: 17, weight: .semibold))
+                            )
+                                .padding(.leading, 30)
+                                .padding(.trailing, 30)
+                        }
+                    }.frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.9)
                 }
             }
         }
@@ -138,6 +137,45 @@ struct GameView: View {
 
 struct Game_Previews: PreviewProvider {
     static var previews: some View {
-        GameView()
+       GeometryReader { geometry in
+           VStack {
+               Spacer()
+              
+                   GameView()
+             
+               Spacer()
+               
+               HStack {
+                   Spacer()
+                   Image(systemName: "book.fill")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .frame(width: geometry.size.width/5, height: 32)
+                       .foregroundColor(.firstColor)
+                       
+                   
+                   Image(systemName: "gamecontroller.fill")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .frame(width: geometry.size.width/5, height: 32)
+
+                       .foregroundColor(.firstColor)
+                      
+                   
+                   Image(systemName: "gear")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .frame(width: geometry.size.width/5, height: 32)
+                       .foregroundColor(.gray)
+                      
+                   
+                   Spacer()
+               }
+               .frame(width: geometry.size.width, height: geometry.size.height/10)
+               .background(Color.white.shadow(radius: 2))
+           }
+           .edgesIgnoringSafeArea(.bottom)
+           .accentColor(.firstColor)
+       }
     }
 }
