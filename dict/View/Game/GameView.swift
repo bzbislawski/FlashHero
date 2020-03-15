319a6a722bc6ce08026c66cfaba1475a79ad4f59
Bartosz Zbislawski <bartoszzbislawski@rentalcars.com>
2020-03-15 16:31:22 +0000

    Compatible for all iPhones

--- dict/View/Game/GameView.swift
+++ dict/View/Game/GameView.swift
@@ -19,36 +19,32 @@ struct GameView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            VStack {
-//                Text("Swipe Game")
-//                    .font(.system(size: 44, weight: .thin))
+            ZStack {
+                Rectangle()
+                    .fill(Color.secondColor)
+                    .overlay(GeometryReader { proxy in
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: proxy.size.width + 1000, height: proxy.size.height + 1000)
+                            .offset(x: (proxy.size.width + 1000) * 0.05, y: -(proxy.size.height + 1000) * 0.52)
+                            .opacity(0.15)
+                        
+                    })
+                    .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.96)
+                    .cornerRadius(25)
+                    .shadow(color: .gray, radius: 12, x: 0, y: 4)
                 
-                ZStack {
-                    Rectangle()
-                        .fill(Color.secondColor)
-                        .overlay(GeometryReader { proxy in
-                            Circle()
-                                .fill(Color.white)
-                                .frame(width: proxy.size.width + 1000, height: proxy.size.height + 1000)
-                                .offset(x: (proxy.size.width + 1000) * 0.05, y: -(proxy.size.height + 1000) * 0.52)
-                                .opacity(0.15)
-                            
-                        })
-                        .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.96)
-                        .cornerRadius(25)
-                        .shadow(color: .gray, radius: 12, x: 0, y: 4)
-                    
-                    
+                
+                
+                VStack {
+                    Image(systemName: "gamecontroller")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .font(.system(size: 56, weight: .bold))
+                        .frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.3)
+                        .foregroundColor(.white)
                     
                     VStack {
-                        Image(systemName: "gamecontroller")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .font(.system(size: 56, weight: .bold))
-                            .frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
-                            .foregroundColor(.white)
-                            .padding(.bottom, geometry.size.height * 0.06)
-                        
                         HStack {
                             Text("Vocabulary")
                                 .italic()
@@ -80,7 +76,9 @@ struct GameView: View {
                             .onTapGesture {
                                 self.showSheet = true
                         }
-                        
+                    }.padding(.bottom, geometry.size.height * 0.03)
+                    
+                    VStack {
                         HStack {
                             Text("Word Order")
                                 .italic()
@@ -108,25 +106,25 @@ struct GameView: View {
                         )
                             .padding(.leading, 30)
                             .padding(.trailing, 30)
-                        
-                        Spacer()
-                        
-                        Button(action: {}) {
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
-                                .background(Color.white)
-                                .cornerRadius(8)
-                                .frame(height: 47)
-                                .overlay(
-                                    Text("Start Game")
-                                        .foregroundColor(Color.quaternaryBackgroundColor)
-                                        .font(.system(size: 17, weight: .semibold))
-                            )
-                                .padding(.leading, 30)
-                                .padding(.trailing, 30)
-                        }
-                    }.frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.9)
-                }
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.secondaryBackgroundColor, lineWidth: 1)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .frame(height: 47)
+                            .overlay(
+                                Text("Start Game")
+                                    .foregroundColor(Color.quaternaryBackgroundColor)
+                                    .font(.system(size: 17, weight: .semibold))
+                        )
+                            .padding(.leading, 30)
+                            .padding(.trailing, 30)
+                    }
+                }.frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.9)
             }
         }
         .sheet(isPresented: $showSheet) {
@@ -137,45 +135,45 @@ struct GameView: View {
 
 struct Game_Previews: PreviewProvider {
     static var previews: some View {
-       GeometryReader { geometry in
-           VStack {
-               Spacer()
-              
-                   GameView()
-             
-               Spacer()
-               
-               HStack {
-                   Spacer()
-                   Image(systemName: "book.fill")
-                       .resizable()
-                       .aspectRatio(contentMode: .fit)
-                       .frame(width: geometry.size.width/5, height: 32)
-                       .foregroundColor(.firstColor)
-                       
-                   
-                   Image(systemName: "gamecontroller.fill")
-                       .resizable()
-                       .aspectRatio(contentMode: .fit)
-                       .frame(width: geometry.size.width/5, height: 32)
-
-                       .foregroundColor(.firstColor)
-                      
-                   
-                   Image(systemName: "gear")
-                       .resizable()
-                       .aspectRatio(contentMode: .fit)
-                       .frame(width: geometry.size.width/5, height: 32)
-                       .foregroundColor(.gray)
-                      
-                   
-                   Spacer()
-               }
-               .frame(width: geometry.size.width, height: geometry.size.height/10)
-               .background(Color.white.shadow(radius: 2))
-           }
-           .edgesIgnoringSafeArea(.bottom)
-           .accentColor(.firstColor)
-       }
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                
+                GameView()
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geometry.size.width/5, height: 32)
+                        .foregroundColor(.firstColor)
+                    
+                    
+                    Image(systemName: "gamecontroller.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geometry.size.width/5, height: 32)
+                        
+                        .foregroundColor(.firstColor)
+                    
+                    
+                    Image(systemName: "gear")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geometry.size.width/5, height: 32)
+                        .foregroundColor(.gray)
+                    
+                    
+                    Spacer()
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height/10)
+                .background(Color.white.shadow(radius: 2))
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .accentColor(.firstColor)
+        }
     }
 }
